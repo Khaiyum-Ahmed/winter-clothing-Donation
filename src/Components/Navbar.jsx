ad6fa504b80a23f6f6e14abcbd8f7a823c3eb399
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-09-03 12:35:14 +0600

    navbar added:

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,38 @@
-
+import {  Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo (1).png"
 const Navbar = () => {
+    const links = <>
+    <li><NavLink to="/">Home</NavLink></li>
+    <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+    <li><NavLink to="/donationCampaigns">Donation campaigns</NavLink></li>
+    <li><NavLink to="/howToHelp">How to Help</NavLink></li>
+       
+    </>
     return (
-        <div>
-            <h1>navlinks</h1>
+        <div className="max-w-11/12 mx-auto py-3">
+            <div className="navbar bg-[#21293b] shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost text-white lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {links}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl"><img src={logo} alt="" /></a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu text-white text-base menu-horizontal px-1">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link to="/auth/login" className="btn">Login</Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Home = () => {
     return (
         <div>
-            <h1>Home section</h1>
+                <nav className="bg-[#21293b]">
+                    <Navbar></Navbar>
+                </nav>
+
+            <header className="max-w-11/12 mx-auto">
+                <Header></Header>
+            </header>
+            <main className="max-w-11/12 mx-auto">
+                <Outlet></Outlet>
+            </main>
+            <footer className="max-w-11/12 mx-auto">
+                <Footer></Footer>
+            </footer>
         </div>
     );
 };
